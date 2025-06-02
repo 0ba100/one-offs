8a5f4465649f86e618fc93837e44190c93cc68eb
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-02 16:18:08 -0600

    remove struct

--- multi-rust/parse_nmap/src/main.rs
+++ multi-rust/parse_nmap/src/main.rs
@@ -3,17 +3,11 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-struct Record {
-    name: String,
-    port: String,
-    prevalence: f64
-}
-
 fn main() -> anyhow::Result<()> {
     let mut reader = BufReader::new(File::open("nmap-services.txt")?);
     let mut line = String::new();
 
-    let mut records: Vec<Record> = Vec::new();
+    let mut records: Vec<(String, String, f64)> = Vec::new();
 
     while reader.read_line(&mut line)? > 0 {
         if &line.chars().nth(0) == &Some('#') {
@@ -23,15 +17,15 @@ fn main() -> anyhow::Result<()> {
 
         let split: Vec<&str> = line.split('\t').collect();
 
-        records.push(Record { name: split[0].to_owned(), port: split[1].to_owned(), prevalence: split[2].to_owned().trim().parse::<f64>()?});
+        records.push((split[0].to_owned(), split[1].to_owned(), split[2].to_owned().trim().parse::<f64>()?));
 
         line.clear();
     }
 
-    records.sort_by(|a, b| b.prevalence.total_cmp(&a.prevalence));
+    records.sort_by(|a, b| b.2.total_cmp(&a.2));
 
     for (i, record) in records.iter().take(30).enumerate() {
-        println!("#{}: {} {} {}", i+1, record.name, record.port, record.prevalence)
+        println!("#{}: {} {} {}", i+1, record.0, record.1, record.2)
     }
 
     Ok(())
