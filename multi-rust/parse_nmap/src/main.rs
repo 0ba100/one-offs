83b2aa3212c3c56683fd4135e50516b9b794569b
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-02 17:32:50 -0600

    sort by port number

--- multi-rust/parse_nmap/src/main.rs
+++ multi-rust/parse_nmap/src/main.rs
@@ -17,15 +17,40 @@ fn main() -> anyhow::Result<()> {
 
         let split: Vec<&str> = line.split('\t').collect();
 
-        records.push((split[0].to_owned(), split[1].to_owned(), split[2].to_owned().trim().parse::<f64>()?));
+        records.push((
+            split[0].to_owned(),
+            split[1].to_owned(),
+            split[2].to_owned().trim().parse::<f64>()?,
+        ));
 
         line.clear();
     }
 
     records.sort_by(|a, b| b.2.total_cmp(&a.2));
 
-    for (i, record) in records.iter().take(30).enumerate() {
-        println!("#{}: {} {} {}", i+1, record.0, record.1, record.2)
+    let mut top_30 = records.iter().take(30).collect::<Vec<_>>();
+
+    top_30.sort_by(|a, b| {
+        a.1.split('/')
+            .next()
+            .unwrap()
+            .trim()
+            .parse::<u32>()
+            .unwrap()
+            .cmp(
+                &b
+                .1
+                .split('/')
+                .next()
+                .unwrap()
+                .trim()
+                .parse::<u32>()
+                .unwrap()
+            )
+    });
+
+    for (i, record) in top_30.iter().enumerate() {
+        println!("#{}: {} {} {}", i + 1, record.0, record.1, record.2)
     }
 
     Ok(())
