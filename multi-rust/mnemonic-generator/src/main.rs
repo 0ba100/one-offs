bddb974d5eda687a95f98beab263c32cd8808409
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-03 13:42:30 -0600

    mnemonic generator draft outline

---
+++ multi-rust/mnemonic-generator/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Goal: translate a series into a mnemonic, generate an image, create an Anki Card.
+    // Provide: Location (Memory Palace: Loce.Subloci)
+    // Provide: text to translate
+    // provide optional mnemonic
+    // provide optional image description
+    // go through steps one by one to translate
+    // Use the major system on numbers
+    // use the anki api to create the card
+    // always sync after creation if possible
+
+    // could use a UI if you wanted: e.g. dioxus/tauri/egui/etc.
+}
