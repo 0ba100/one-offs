af4eeaa47a0099d49d7031de91abeb31109cf904
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-04 17:09:36 -0600

    playing around with policysets

--- multi-rust/cedarbox/src/main.rs
+++ multi-rust/cedarbox/src/main.rs
@@ -1,20 +1,20 @@
-use std::str::FromStr;
 use anyhow::Context;
+use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let temporary_file = tempfile::NamedTempFile::new().context("Failed to create temp file")?;
-    let path = temporary_file.path().to_str().context("invalid temporary file")?;
-    let pool = sqlx::SqlitePool::connect(path).await.context("Failed to init sqlite db")?;
+    // let temporary_file = tempfile::NamedTempFile::new().context("Failed to create temp file")?;
+    // let path = temporary_file.path().to_str().context("invalid temporary file")?;
+    // let pool = sqlx::SqlitePool::connect(path).await.context("Failed to init sqlite db")?;
 
-    sqlx::migrate!("./migrations")
-        .run(&pool)
-        .await
-        .context("Failed to run migrations")?;
+    // sqlx::migrate!("./migrations")
+    //     .run(&pool)
+    //     .await
+    //     .context("Failed to run migrations")?;
 
-    seed_database(&pool).await.context("Failed to seed database")?;
+    // seed_database(&pool).await.context("Failed to seed database")?;
 
-    test_cedar();
+    test_cedar()?;
 
     Ok(())
 }
@@ -27,17 +27,31 @@ async fn seed_database(pool: &sqlx::SqlitePool) -> anyhow::Result<()> {
         .await
         .context("Failed to insert organization")?;
 
-    sqlx::query("INSERT INTO users (id, name, organization_id) VALUES (?, 'Alice', ?), (?, 'Bob', ?)")
-        .bind::<String>(uuid::Uuid::new_v4().into())
-        
-        .execute(pool)
-        .await
-        .context("Failed to insert users")?;
+    sqlx::query(
+        "INSERT INTO users (id, name, organization_id) VALUES (?, 'Alice', ?), (?, 'Bob', ?)",
+    )
+    .bind::<String>(uuid::Uuid::new_v4().into())
+    .execute(pool)
+    .await
+    .context("Failed to insert users")?;
     Ok(())
 }
 
+fn test_cedar() -> anyhow::Result<()> {
+    let policy_text = std::fs::read_to_string("./policy.cedar")?;
+    let template_policy = cedar_policy::PolicySet::from_str(&policy_text)?;
+    let num = template_policy
+        .templates();
+        // .map(|t| t.)
+        // .flat_map(|t| t.slots().map(|s| s.to_string()))
+        // .fold(String::new(), |s, s2| s + &s2);
+    // let slots = template_policy.slots().map(|s| s.to_string()).collect::<Vec<String>>();
 
-fn test_cedar() {
+    // println!("{:?}", num);
+    // println!("{:?}", slots);
+
+    return Ok(());
+
     const POLICY_SRC: &str = r#"
     permit (
         principal in Role::"supervisor",
@@ -134,8 +148,10 @@ fn test_cedar() {
 
     let action = cedar_policy::EntityUid::from_str(r#"Action::"approveSchedule""#).unwrap();
 
-    let resource_in_scope = cedar_policy::EntityUid::from_str(r#"Schedule::"scheduleInScope""#).unwrap();
-    let resource_out_of_scope = cedar_policy::EntityUid::from_str(r#"Schedule::"scheduleOutOfScope""#).unwrap();
+    let resource_in_scope =
+        cedar_policy::EntityUid::from_str(r#"Schedule::"scheduleInScope""#).unwrap();
+    let resource_out_of_scope =
+        cedar_policy::EntityUid::from_str(r#"Schedule::"scheduleOutOfScope""#).unwrap();
 
     let context = cedar_policy::Context::from_json_value(context_json, None).unwrap();
 
@@ -145,9 +161,12 @@ fn test_cedar() {
     let authorizer = cedar_policy::Authorizer::new();
 
     let fred_request_in_scope =
-        cedar_policy::Request::new(fred_principal, action, resource_in_scope, context, None).unwrap();
+        cedar_policy::Request::new(fred_principal, action, resource_in_scope, context, None)
+            .unwrap();
 
     let decision = authorizer.is_authorized(&fred_request_in_scope, &policy_set, &entities);
 
     println!("{:?}", decision.decision());
+
+    Ok(())
 }
