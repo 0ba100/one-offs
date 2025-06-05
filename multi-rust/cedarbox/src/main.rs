0d7aee6360702f5a07e18e703c69b4f47eeee069
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-05 16:21:15 -0600

    some attempts at templating

--- multi-rust/cedarbox/src/main.rs
+++ multi-rust/cedarbox/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
-use std::str::FromStr;
+use itertools::Itertools;
+use std::{collections::HashMap, hash::Hash, str::FromStr};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -40,12 +41,36 @@ async fn seed_database(pool: &sqlx::SqlitePool) -> anyhow::Result<()> {
 fn test_cedar() -> anyhow::Result<()> {
     let policy_text = std::fs::read_to_string("./policy.cedar")?;
     let template_policy = cedar_policy::PolicySet::from_str(&policy_text)?;
-    let num = template_policy
-        .templates();
-        // .map(|t| t.)
-        // .flat_map(|t| t.slots().map(|s| s.to_string()))
-        // .fold(String::new(), |s, s2| s + &s2);
+    let mut role_policy = cedar_policy::PolicySet::new();
+    for policy in template_policy.policies() {
+        role_policy.add(policy.clone())?;
+    }
+
+    let role_uid = cedar_policy::EntityUid::from_str("Role::\"supervisor\"")?;
+    for template in template_policy.templates() {
+        let template_mapping: HashMap<cedar_policy::SlotId, cedar_policy::EntityUid> = template
+            .slots()
+            .unique_by(|s| s.to_string())
+            .map(|s| (s.clone(), role_uid.clone()))
+            .collect();
+
+        role_policy.link(
+            template.id().clone(),
+            cedar_policy::PolicyId::new(template.id()),
+            template_mapping,
+        )?;
+    }
+
+    // let templates = template_policy.templates().map(|t| )
+
+    // let num = template_policy.link(template_id, new_id, vals);
+    // .map(|t| t.)
+    // .flat_map(|t| t.slots().map(|s| s.to_string()))
+    // .fold(String::new(), |s, s2| s + &s2);
     // let slots = template_policy.slots().map(|s| s.to_string()).collect::<Vec<String>>();
+
+    // let attached_policy = cedar_policy::Policy::template_links(&self);
+    // some sort of map from policy to template id.
 
     // println!("{:?}", num);
     // println!("{:?}", slots);
