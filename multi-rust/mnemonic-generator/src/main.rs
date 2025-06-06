0852e3d2b2301dd8a0327c4ee2ec6c10df62b988
Cabbage <wqgcwxmc@anonaddy.com>
2025-06-06 17:59:02 -0600

    get some bytes

--- multi-rust/mnemonic-generator/src/main.rs
+++ multi-rust/mnemonic-generator/src/main.rs
@@ -1,6 +1,9 @@
+use std::{thread::sleep, time::Duration};
+
+use anyhow::{anyhow, Result as AR};
 use dioxus::prelude::*;
-use anyhow::{Result as AR, anyhow};
-use openai_api_rust::{images::{ImagesApi, ImagesBody}, Auth, OpenAI};
+use dioxus_sdk::utils::timing::use_interval;
+use serde_json::Value;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -21,29 +24,130 @@ fn App() -> Element {
 }
 
 #[component]
+fn ProgressIndicator() -> Element {
+    let mut prg = use_signal(|| 0.1);
+    _ = use_interval(Duration::from_millis(50), move || {
+        if prg.cloned() < 100.0 {
+            prg.set(prg.cloned() + 0.1);
+        }
+    });
+
+    rsx! {
+        div { class: "w-full h-2 bg-gray-200 rounded-full",
+            div { class: "h-full bg-blue-500 rounded-full", style: "width: {prg}%" }
+        }
+    }
+}
+
+#[component]
 pub fn Hero() -> Element {
     let img_description = use_signal(|| String::new());
-    let mut img_url: Signal<Option<String>> = use_signal(|| None);
+    let mut img_src: Signal<Option<String>> = use_signal(|| None);
+    let mut active_request = use_signal(|| false);
 
     let generate_image = move |_: Event<MouseData>| async move {
-        let body = ImagesBody { 
-            prompt: img_description.cloned(),
-            n: None, 
-            size: None, 
-            response_format: None, 
-            user: Some(String::from("mnemonics-generator-dev")),
-        };
-        
-        // this is blocking rofl
-        let auth = Auth::from_env()
-            .map_err(|e| anyhow!("Failed to load auth: {}", e))
-            .map(|auth| OpenAI::new(auth, "https://api.openai.com/v1"))
-            .and_then(|c| c.image_create(&body).map_err(|e| anyhow!("{e}")))
-            .ok()
-            .and_then(|r| r.data)
-            .and_then(|d| d.first().map(|f| f.url.to_owned()));
+        active_request.set(true);
 
-        img_url.set(auth);
+        let task_req = std::env::var("BFL_API_KEY").map(|k| {
+            reqwest::Client::new()
+                .post("https://api.bfl.ai/v1/flux-kontext-max")
+                .header("x-key", k)
+                .header("Content-Type", "application/json")
+                .body(
+                    serde_json::json!({
+                     "prompt": img_description.cloned(),
+                     "output_format": "png",
+                     "prompt_upsampling": true
+                    })
+                    .to_string(),
+                )
+        });
+
+        let task_res = match task_req {
+            Ok(req) => req.send().await.map_err(|e| anyhow!("{e}")),
+            Err(e) => Err(anyhow!("{e}")),
+        };
+
+        let task_body = match task_res {
+            Ok(res) => res.text().await.map_err(|e| anyhow!("{e}")),
+            Err(e) => Err(anyhow!("{e}")),
+        };
+
+        #[derive(serde::Deserialize, Clone)]
+        struct QueuedTask {
+            id: String,
+            polling_url: String,
+        }
+
+        #[derive(serde::Deserialize)]
+        struct PollResponse {
+            status: String,
+            result: Option<PollResult>
+        }
+
+        #[derive(serde::Deserialize)]
+        struct PollResult {
+            sample: Option<String>,
+        }
+
+        let task =
+            task_body.and_then(|v| serde_json::from_str::<QueuedTask>(&v).map_err(|e| anyhow!("{e}"))).ok();
+
+        let mut result: Option<String> = None;
+        let mut iteration_count = 30;
+        while result.is_none() {
+            if iteration_count <= 0 {
+                break;
+            }
+            iteration_count -= 1;
+            let poll_request = task
+                .clone()
+                .ok_or(anyhow!("no task"))
+                .and_then(|v| {
+                    std::env::var("BFL_API_KEY")
+                        .map(|k| (k, v))
+                        .map_err(|e| anyhow!("{e}"))
+                })
+                .map(|(key, value)| {
+                    reqwest::Client::new()
+                        .get(value.polling_url)
+                        .header("accept", "application/json")
+                        .header("x-key", key)
+                });
+
+            let poll_response = match poll_request {
+                Ok(s) => s.send().await.map_err(|e| anyhow!("{e}")),
+                Err(e) => Err(anyhow!("{e}"))
+            };
+
+            let poll_body = match poll_response {
+                Ok(r) => r.text().await.map_err(|e| anyhow!("{e}")),
+                Err(e) => Err(anyhow!("{e}"))
+            };
+
+            let value = poll_body.and_then(|b| serde_json::from_str::<PollResponse>(&b).map_err(|e| anyhow!("{e}")));
+
+            if value.as_ref().is_ok_and(|v| v.status == "Ready") {
+                result = value.ok().and_then(|v| v.result).and_then(|r| r.sample)
+            }
+
+            tokio::time::sleep(Duration::from_secs(3)).await
+        }
+
+        let retrieval_request = result.map(|url| reqwest::Client::new().get(url));
+
+        let img_res = match retrieval_request {
+            Some(r) => r.send().await.ok(),
+            None => None,
+        };
+
+        let img_bytes = match img_res {
+            Some(r) => r.bytes().await.ok(),
+            None => None,
+        };
+
+        img_src.set(None);
+        active_request.set(false);
     };
 
     rsx! {
@@ -120,15 +224,19 @@ pub fn Hero() -> Element {
                             class: "w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
                             id: "imageDescription",
                         }
+                        if active_request() {
+                            ProgressIndicator {}
+                        }
                         button {
                             onclick: generate_image,
                             r#type: "button",
-                            class: "mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500",
+                            disabled: active_request(),
+                            class: "mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-500",
                             "Generate"
                         }
                         div { class: "mt-4",
                             img {
-                                src: img_url,
+                                src: img_src,
                                 alt: "Generated image placeholder",
                                 class: "w-full h-auto border border-gray-300 rounded-md",
                                 id: "imagePreview",
